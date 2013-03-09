a4c4794e17472bb4aceb128f6d7a1158d339208b
saziemucai <saziemucai@gmail.com>
2013-03-10 07:35:49 +0800

    home page edit.

--- assets/www/index.php
+++ assets/www/index.php
@@ -6,16 +6,24 @@
 	<link rel="stylesheet" href="css/jquery.mobile.min.css" />
 	<script src="js/jquery.min.js"></script>
 	<script src="js/jquery.mobile.min.js"></script>
-</head> 
+	
+	<script src="text/css">
+	a { text-decoration: underline}
+	</script>
+	</head> 
 		<body background="images/bg.jpg">
 
 <div data-role="page">
 
+<br><br>
+<center>
+<img src="images/logo.jpg">
+</center>
 
 	<div data-role="content">	
 		<p><ul data-role="listview">
 		
-			<br><br><br><br><br><br>
+		<br>
 
 		<a href="rev-view.php"><button><font color="#000"><b>Revenues</b></font></button></a>
 		<a href="exp-view.php"><button><font color="#000"><b>Expenses</b></font></button></a>
