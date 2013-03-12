3ccfb5c0f9d08c2148da9eae03bf35aa6bc3b645
saziemucai <saziemucai@gmail.com>
2013-03-12 18:17:09 +0800

    pre-final homepage(index.php) and new bg image

--- assets/www/index.php
+++ assets/www/index.php
@@ -2,39 +2,39 @@
 <html> 
 	<head> 
 
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet" href="css/jquery.mobile.min.css" />
-	<script src="js/jquery.min.js"></script>
-	<script src="js/jquery.mobile.min.js"></script>
 	
-	<script src="text/css">
-	a { text-decoration: underline}
-	</script>
-	</head> 
-		<body background="images/bg.jpg">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+</head> 
 
-<div data-role="page">
+<body background="images/bg.jpg">
 
-<br><br>
-<center>
-<img src="images/logo.jpg">
-</center>
 
-	<div data-role="content">	
-		<p><ul data-role="listview">
+<div data-role="page" class="type-home">
+<div data-role="content">	
 		
-		<br>
+	
+<br>
 
-		<a href="rev-view.php"><button><font color="#000"><b>Revenues</b></font></button></a>
-		<a href="exp-view.php"><button><font color="#000"><b>Expenses</b></font></button></a>
-		<a href="summary-rev.php"><button><font color="#000"><b>Summary</b></font></button></a>
-		<a href="bookingsched.php"><button><font color="#000"><b>Booking Schedule</b></font></button></a>
-		<a href="cus-add.php"><button><font color="#000"><b>Customer's Database</b></font></button>
-
-</ul></p>		
+<center>
+<img src="images/logo.jpg" height="130px" width="275px">
+</center>
+	<br>
+<div class="content-primary">
+<ul data-role="listview" data-inset="true" data-theme="a">
+			<li><a href="rev-view.php">Revenues</a></li>
+			<li><a href="exp-view.php">Expenses</a></li>
+			<li><a href="summary-rev.php">Summary</a></li>
+			<li><a href="bookingsched.php">Booking Schedule</a></li>
+			<li><a href="cus-add.php">Customer's Database</a></li>
+		</ul>
+		
 	</div><!-- /content -->
 
 </div><!-- /page -->
+</div>
 
 </body>
 </html>
