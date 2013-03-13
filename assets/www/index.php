1b444a53b65517be85979bdb97ff4ff359772378
saziemucai <saziemucai@gmail.com>
2013-03-13 08:31:51 +0800

    final homepage (index.php)

--- assets/www/index.php
+++ assets/www/index.php
@@ -2,30 +2,38 @@
 <html> 
 	<head> 
 
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/jquery.mobile.min.css" />
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.mobile.min.js"></script>
 </head> 
-		<body background="images/black.jpg" align="center">
 
-<div data-role="page">
+<body>
 
+<div data-role="page" class="type-home" style="background-image: url('images/bg.jpg'); background-attachment: fixed; background-repeat: no-repeat; background-size: 100% 100%;">
+<div data-role="content">	
+		
+	
+<br><br>
 
-	<div data-role="content">	
-		<p><ul data-role="listview" data-inset="true" data-filter="true">
-
-
-		<a href="rev-view.php"><button class="btn btn-warning" style="width:170px;"><font color="#000"><b>Revenues</b></font></button></a>
-		<a href="exp-view.php"><button class="btn btn-warning" style="width:170px;"><font color="#000"><b>Expenses</b></font></button></a>
-		<a href="summary-rev.php"><button class="btn btn-warning" style="width:170px;"><font color="#000"><b>Summary</b></font></button></a>
-		<a href="bookingsched.php"><button class="btn btn-warning" style="width:170px;"><font color="#000"><b>Booking Schedule</b></font></button></a>
-		<a href="cus-add.php"><button class="btn btn-warning" style="width:170px;"><font color="#000"><b>Customer's Database</b></font></button>
-
-</ul></p>		
+<center>
+<img src="images/logo.jpg" height="130px" width="275px">
+</center>
+			<br>
+<div class="content-primary">
+<ul data-role="listview" data-inset="true" data-theme="a">
+			<li><a href="rev-view.php">Revenues</a></li>
+			<li><a href="exp-view.php">Expenses</a></li>
+			<li><a href="summary-rev.php">Summary</a></li>
+			<li><a href="bookingsched.php">Booking Schedule</a></li>
+			<li><a href="cus-add.php">Customer's Database</a></li>
+		</ul>
+		
 	</div><!-- /content -->
 
 </div><!-- /page -->
+</div>
 
 </body>
 </html>
