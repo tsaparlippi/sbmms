3572fffd12a1ced38b6c0e2ff922ee90c9d5f343
saziemucai <saziemucai@gmail.com>
2013-03-13 08:07:49 +0800

    rev-view.php

--- assets/www/rev-view.php
+++ assets/www/rev-view.php
@@ -11,9 +11,9 @@ $result = mysql_query("SELECT * FROM revenues");
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
-	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script> 
+	<link rel="stylesheet" href="css/jquery.mobile.min.css" />
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.mobile.min.js"></script>
 	
 	
 	<script src="lib/jquery.js" type="text/javascript"></script>
