60c737c9e8f54e37306d32b88b3cdcc0e1d9cb0d
jane faith abanid <janefaith.abanid@gmail.com>
2013-03-20 23:48:22 +0800

    edited charter & summary, added custom_summary
    edited summary.php and added custom_summary.php for monthly summary.
    edited feature list, use case,  test case

--- assets/www/summary-rev.php
+++ assets/www/summary-rev.php
@@ -22,49 +22,41 @@
 		
 		<?php } ?>
 
+		<form action="custom_summary.php" method="POST">
+		<select name="month">
+		<option value="0"></option>
+		<option value="1">January</option>
+		<option value="2">February</option>
+		<option value="3">March</option>
+		<option value="4">April</option>
+		<option value="5">May</option>
+		<option value="6">June</option>
+		<option value="7">July</option>
+		<option value="8">August</option>
+		<option value="9">September</option>
+		<option value="10">October</option>
+		<option value="11">November</option>
+		<option value="12">December</option>
+		</select>
+		<select name="year">
+		<option value="2010">2010</option>
+		<option value="2011">2011</option>
+		<option value="2012">2012</option>
+		<option value="2013">2013</option>
+		<option value="2013">2014</option>
+		<option value="2014">2014</option>
+		<option value="2015">2015</option>
+		<option value="2015">2016</option>
+		<option value="2015">2017</option>
+		<option value="2015">2018</option>
+		<option value="2015">2019</option>
+		<option value="2015">2020</option>
 		
-		<center>
+		</select>
+		<input type="submit" name="submit" value="Proceed" />
+		</form>
 		
-			<div id="dt_example" style="width: 860px;">
-				<div id="container" style="width: 8">
-				<div class="demo_jui" style="width: 860px;">
 		
-	<tbody>
-			<?php $rev = mysql_query("SELECT SUM(cost) as totalcost FROM revenues") or die(mysql_error());  ?>
-			<?php WHILE($revs = mysql_fetch_array($rev)){
-				$totalcost = $revs['totalcost']; }?>
-			
-			<?php $exp = mysql_query("SELECT SUM(cost2) as totalexp FROM expenses") or die(mysql_error()); ?>
-			<?php WHILE($exps = mysql_fetch_array($exp)){
-				$totalexp = $exps['totalexp']; }
-				?>
-
-			<tr class="gradeA del<?php echo $id;?>">
-			<form action="editrev.php" method="POST">
-			<br/><br/><br/>
-			<center>
-	<div class="ui-grid-a ui-responsive">
-    <div class="ui-block-a"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center">Total Revenues</font></div></div>
-    <div class="ui-block-b"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center"><?php echo $totalcost; ?></font></div></div>
-    <div class="ui-block-a"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center">Total Expenses</font></div></div>
-    <div class="ui-block-b"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center"><?php echo $totalexp; ?></font></div></div>
-    <div class="ui-block-a"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center">Income</font></div></div>
-    <div class="ui-block-b"><div class="ui-body ui-body-d" style="height:60px"><font size="5px" align="center"><?php echo $totalcost-$totalexp; ?></font></div></div>
-	</div>	</center>
-				
-				<!-- hide actions for user access level-->
-				<?php if($_SESSION['user_type'] != 'User'){ ?>
-				<!-- hide actions for user access level-->
-				 
-					
-					</form>
-					<?php } ?>
-				</td>
-				
-				</tr>
-	</tbody>
-
-</center>
 		
 		</div></div></div></div>
 </body>
