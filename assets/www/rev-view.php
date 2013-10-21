be19781225f12c2de1daa9a466566945f000c20a
saziemucai <saziemucai@gmail.com>
2013-10-22 02:54:06 +0800

    updated revenues
    (rev-view.php) changed datatables to listview with filter

--- assets/www/rev-view.php
+++ assets/www/rev-view.php
@@ -5,61 +5,29 @@
 	<link rel="stylesheet" href="css/jquery.mobile.min.css" />
 	<script src="js/jquery.min.js"></script>
 	<script src="js/jquery.mobile.min.js"></script>
-
-		
+	
+	
 <body>
 
 	<div data-role="page" class="type-home" 
-			style="background-image: url('images/bg.jpg'); 
-					background-attachment: fixed; 
+			style="background: #FFAC2F;
 					background-repeat: repeat; 
 					background-size: 100% 100%;">
-					
-		<style type="text/css" title="currentStyle">
-			@import "css/demo_page.css";
-			@import "css/demo_table_jui.css";
-		</style>
-		
-		<script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
-	
-		<script type="text/javascript" charset="utf-8">
-			$(document).ready(function() {
-				oTable = $('#example').dataTable({
-					"bJQueryUI": true,
-					"sPaginationType": "full_numbers"
-				});
-			} );
-		</script>
-		
+						
 
 		<div data-role="header" data-position="inline">
 			<a href="index.php" data-icon="arrow-l" rel="external">Back</a>
 			<h1>Revenues</h1>
-			<a href="addrev.php" data-rel="dialog" data-transition="pop" data-role="button" data-theme="a" data-icon="plusthick"  data-inline="true" rel="external">Add</a>
 		</div>
-			
+		
 		
 			<div data-role="content">
+			
+			
 				
 				<center>
-					
-					<div id="dt_example">
-									
-					<table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
-
-						<thead>
-							<tr>
-								<th>Date</th>
-								<th style="width:30px">Event</th>
-								<th>Customer</th>
-								<th>Amount</th>
-						
-							</tr>
-						</thead>
-					
-						<tbody>
-					
-							<?php
+				
+						<?php
 								include("config1.php");
 							
 								$revenues = mysql_query("SELECT * FROM guest WHERE Amount != '0'") ?>
@@ -79,27 +47,23 @@
 		
 									}
 								}
+								
+								echo '<ul data-role="listview" data-filter="true" data-filter-placeholder="Search revenues..." data-inset="true">';
 		
 								$db = mysql_query("SELECT * FROM revenues ORDER BY Guest_id ASC ");
 									while($row = mysql_fetch_array($revenues)){ 
 					
-										echo '<tr>';
-											echo '<td>'.$row['TDate'].'</td>';
-											echo '<td>'.$row['Company'].'</td>';
-											echo '<td>'.$row['Name'].'</td>';
-											echo '<td align="right">'.$row['Amount'].'</td>';
-										echo '</tr> ';
-							
+											echo '<li>';
+											echo '<h2>'.$row['Name'].'</h2>';
+											echo '<p> <font color="green">'.$row['TDate'].' | '.$row['Company'].' | P'.$row['Amount'].'</font> </a>';
+											echo ' </li>';
+      					
+										
 								}
-							?>
-				
-						</tbody>
-					</table>
-					
-					</div>
+									
+									echo '</ul>';
+										?>
+							
 
-	</div><!-- /page -->
-		
-				
 </body>
 </html>
