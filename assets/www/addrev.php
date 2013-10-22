b832961ad35b62a08029a1a0e784b5bb6b7ed919
saziemucai <saziemucai@gmail.com>
2013-10-22 20:57:47 +0800

    added 'add revenues and save revenues'
    (fixing errors)

--- assets/www/addrev.php
+++ assets/www/addrev.php
@@ -46,9 +46,9 @@
 
 
 	Personal Information<br>
-			<input required title="*Please Enter Your Full Name" class="sf" type="text" name="name" placeholder="Complete Name">
-	<input title="Your Address. Must Have Brgy. City eg. Salvacion Panabo City." required class="sf"  type="text"   name="Address" placeholder="Address">
-<input pattern=".{6,}" title="Your Mobile No. Must be 11 digit number.  eg. format 09092967492" required  class="sf" type="text"   name="Contact_no" placeholder="Contact_no" maxlength="11" onKeyPress="return isNumberKey(event)">
+			<input required title="*Please Enter Your Full Name" class="sf" type="text" name="Name" id="Name" placeholder="Complete Name">
+	<input title="Your Address. Must Have Brgy. City eg. Salvacion Panabo City." required class="sf"  type="text"   id="Address" name="Address" placeholder="Address">
+<input pattern=".{6,}" title="Your Mobile No. Must be 11 digit number.  eg. format 09092967492" required  class="sf" type="text"   name="Contact_no" id="Contact_no" placeholder="Contact_no" maxlength="11" onKeyPress="return isNumberKey(event)">
 
 
 <div data-role="fieldcontain">
--- assets/www/save_rev.php
+++ assets/www/save_rev.php
@@ -4,14 +4,14 @@ include('config1.php');
 
 $Guest_id = $_POST['Guest_id'];
 $TDate = $_POST['TDate'];
-$name= $_POST['name'];
+$Name= $_POST['Name'];
 $Address= $_POST['Address'];
 $Contact_no= $_POST['Contact_no'];
 $Company = $_POST['Company'];
 $venue = $_POST['venue'];
 $Amount = $_POST['Amount'];
 	
-mysql_query("INSERT INTO guest (TDate, Name, Address, Contact_no, Company, venue, Amount) VALUES('$TDate', '$name', '$Address', '$Contact_no','$Company', '$venue','$Amount')"); 
+mysql_query("INSERT INTO guest (TDate, Name, Address, Contact_no, Company, venue, Amount) VALUES('$TDate', '$Name', '$Address', '$Contact_no','$Company', '$venue','$Amount')"); 
 
 header("location: rev-view.php");
 			?>
