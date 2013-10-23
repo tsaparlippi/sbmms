d4fb4f8efc9d51a43a047cede250f8366f44378e
janefaith <janefaith.abanid@gmail.com>
2013-10-23 12:21:15 -0700

    added validation for pop-up on submit form

--- assets/www/pop-uponsubmit.php
+++ assets/www/pop-uponsubmit.php
@@ -51,6 +51,37 @@ $(this).attr("checked",status);
 
 	</script>
 
+	 <script type="text/javascript">
+$(document).on('pageinit', function () {
+
+    $("#form").validate({
+        rules: {
+            name: "required",
+            Address: "required",
+			Contact_no: {
+				required: true,
+					minlength: 11,
+					maxlength: 11
+				},
+				
+			Company: "required",
+			Payment: "required",
+			multi[]: "required",
+			Amount: {
+			 required: true,
+                maxlength: 4
+				
+			}
+        },
+        submitHandler: function (form) { // for demo
+            sfkshfjdshfjdsfhd; // for demo
+            return false; // for demo
+        }
+    });
+
+});
+</script>
+
 	</head>
 	
 <body>
@@ -58,7 +89,7 @@ $(this).attr("checked",status);
 <div data-role="dialog">
 
 <div data-role="header" data-position="inline">
-			<a href="Reservation.php" rel="external" data-icon="arrow-l">Back</a>
+			
 			<h1>Reservation</h1>
 		
 			</div>
@@ -102,15 +133,14 @@ $gabie=$difference;
   $thevacantroom = mysql_num_rows($query2); // count the number of vacant room
 
 if( $thevacantroom>0){	
-	?><form action="try3.php"  onsubmit="return ValidateForm1(this)" id="form" method="post" target="_self">
+	?><form action="try3.php"  onsubmit="return ValidateForm1(this)" id="form" name="form" method="post" target="_self">
 		<center><div data-role="fieldcontain">
-			<input name="TDate" value= "<?php $time_zone = 'Asia/Singapore'; date_default_timezone_set($time_zone); echo date("Y-m-d"); ?>" id="TDate" class="hide"><br>
 			Personal Information<br><br>
-			<input required title="*Please Enter Your Full Name" class="sf" type="text" name="name" placeholder="Complete Name"><br>
-			<input title="Your Address. Must Have Brgy. City eg. Salvacion Panabo City." required class="sf"  type="text"   name="Address" placeholder="Address"><br>
-			<input pattern=".{6,}" title="Your Mobile No. Must be 11 digit number.  eg. format 09092967492" required  class="sf" type="text"   name="Contact_no" placeholder="Contact_no" maxlength="11" onKeyPress="return isNumberKey(event)"><br>
-			<select name="Company" placeholder="Company" required="required">
-				<option required title="*Please Enter Company" class="sf"  value=""> Choose event</option>
+			<input class="required" type="text" name="name" placeholder="Complete Name"><br>
+			<input class="required"  type="text"   name="Address" placeholder="Address"><br>
+			<input class="required" type="text"   name="Contact_no" placeholder="Contact_no" maxlength="11" onKeyPress="return isNumberKey(event)"><br>
+			<select name="Company" placeholder="Company" class="required" >
+				<option value=""> Choose event</option>
 				<option >Acquaintance Party
 				<option>Anniversary
 				<option>Baptism
@@ -127,14 +157,14 @@ if( $thevacantroom>0){
 				<option>Seminar
 			<option>
 			</select><br>
-			<select name="Payment" placeholder="Payment" id="Payment" required="required">
-				<option required title="*Please Enter Status" class="sf"  value=""> Choose status</option>
+			<select name="Payment" placeholder="Payment" id="Payment" class="required">
+				<option value=""> Choose status</option>
 				<option value="Full Payment">Full Payment </option>
 				<option value="Initial Payment">Initial Payment </option>
 				<option value="Inquiry">Inquiry </option>			
 			</select><br>
 
-			<input type="text" name="Amount" id="Amount" placeholder="Amount" maxlength="4" onKeyPress="return isNumberKey(event)"><br><br>
+			<input type="text" name="Amount" id="Amount" placeholder="Amount" onKeyPress="return isNumberKey(event)" class="required"><br><br>
 					
 			<input type="submit" data-role="button" data-theme="b" id="button1" value="Reserve" /></div></center><?php
 
